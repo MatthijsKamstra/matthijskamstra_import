07bc1bfa1de9c5741cbd0bd39729e3a4892dc6fb
matthijskamstra <matthijskamstra@5317beb8-914d-0410-8a95-9192322ba915>
2009-12-08 13:22:52 +0000

    renaming

--- css/script/default.js
+++ css/script/default.js
@@ -53,6 +53,26 @@ function openFullScreen(targetURL){
 function Popup(url) {
 	window.open(url,'name','width=780,height=550,scrollbars=no,resizable=yes,toolbar=no,location=no');
 }
+/*
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Idea by:  Nic Wolfe -->
+<!-- This script and many more are available free online at -->
+<!-- The JavaScript Source!! http://javascript.internet.com -->
+
+<!-- Begin
+*/
+/**
+example:
+<a href="#" onClick="javascript:popUp('program_nuna.html')" alt="Bekijk programma Nuna event" />
+*/
+function popUp(URL) {
+	day = new Date();
+	id = day.getTime();
+	eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width=350,height=500,left = 465,top = 237');");
+}
+// End -->
+/*</script>*/
+
 
 
 function Mailto(subject , body){
